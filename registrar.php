776e91bae428d6abe81e838778a653624b46c630
Almaher2812 <alejandromateus2812@gmail.com>
2024-05-31 14:04:05 -0500

    .

--- registrar.php
+++ registrar.php
@@ -1,28 +1,28 @@
 <?php
-    include("conexion.php");
-    if (isset($_POST['register'])){
-        if(
-            strlen($_POST['name']) >= 1 &&
-            strlen($_POST['email']) >= 1 &&
-            strlen($_POST['number']) >= 1 &&
-            strlen($_POST['pasword']) >= 1
-            ){
-                $name = trim($_POST['name']);
-                $email = trim($_POST['email']);
-                $phone = trim($_POST['number']);
-                $pasword = trim($_POST['pasword']);
-                $consulta = "INSERT INTO datos(nombre, email, numero, pasword)
-                    VALUES('$name','$email','$phone','$pasword')";
-                $resultado = mysqli_connect($conex, $consulta);
-                if ($resultado) {
-                    ?>
-                    <h3 class="succes" >Tu resgistro se a completado</h3>
-                    <?php
-                } else{
-                    ?>
-                    <h3 class="succes" >Ocurrio un error</h3>
-                    <?php
-                }
-        }
+include("conexion.php");
+if (isset($_POST['register'])){
+    if(
+        strlen($_POST['name']) >= 1 &&
+        strlen($_POST['email']) >= 1 &&
+        strlen($_POST['number']) >= 1 &&
+        strlen($_POST['pass']) >= 1
+        ){
+            $name = trim($_POST['name']);
+            $email = trim($_POST['email']);
+            $phone = trim($_POST['number']);
+            $password = trim($_POST['pass']);
+            $consulta = "INSERT INTO datos(nombre, email, numero, pasword)
+                VALUES('$name','$email','$phone','$password')";
+            $resultado = mysqli_query($conex, $consulta); // Usando la variable de conexión $conex
+            if ($resultado) {
+                ?>
+                <h3 class="success" >Tu registro se ha completado</h3>
+                <?php
+            } else{
+                ?>
+                <h3 class="error" >Ocurrió un error</h3>
+                <?php
+            }
     }
-<?
+}
+?>
