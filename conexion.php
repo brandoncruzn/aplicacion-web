2f62df9da2d58cb10508b42dcec1c036388bb7de
brandoncruzn <branjon.09@gmail.com>
2024-06-05 15:42:39 -0500

    Update conexion.php
    Signed-off-by: brandoncruzn <branjon.09@gmail.com>

--- conexion.php
+++ conexion.php
@@ -1,14 +1,15 @@
 <?php
-$servername = "viaduct.proxy.rlwy.net";
-$database = "railway";
-$username = "root";
-$password = "oiUfJbGYmCHxFQAMzvxRJdwBfNgiVgcg";
+    $hostname="viaduct.proxy.rlwy.net";
+	$username="root";
+	$password="oiUfJbGYmCHxFQAMzvxRJdwBfNgiVgcg";
+	$dbname="railway";
+	$usertable="personas"
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-// Check connection
-if (!$conn) {
+$con = mysqli_connect('HOSTNAME','USERNAME','PASSWORD'); o morir ("html>script language='JavaScript'>alert('¡No es posible conectarse a la base de datos! Vuelve a intentarlo más tarde.'),history.go(-1)/script>/html>");
+	mysqli_select_db($dbname);
+	if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
-mysqli_close($conn);
+mysqli_close($con);
 ?>
