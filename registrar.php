dd239841b110c315d81f74d33d04e30bb220bfe6
brandoncruzn <branjon.09@gmail.com>
2024-06-05 15:04:41 -0500

    Update registrar.php
    Signed-off-by: brandoncruzn <branjon.09@gmail.com>

--- registrar.php
+++ registrar.php
@@ -1,27 +1,31 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    include("base_de_datos.php");
+#Si todo va bien, se ejecuta esta parte del código...
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $telefono = $_POST['number'];
-    $pass = $_POST['pass'];
-    $cpass = $_POST['cpass'];
+include_once "base_de_datos.php";
+$name = $_POST['name'];
+$email = $_POST['email'];
+$telefono = $_POST['number'];
+$pass = $_POST['pass'];
+$cpass = $_POST['cpass'];
 
-    if ($pass != $cpass) {
+if ($pass != $cpass) {
         echo "Las contraseñas no coinciden.";
     } else {
         $hashed_password = password_hash($pass, PASSWORD_DEFAULT);
 
-        $sql = "INSERT INTO datos (nombre, email, numero, password) VALUES ('$name', '$email', '$number', '$hashed_password')";
+/*
+	Al incluir el archivo "base_de_datos.php", todas sus variables están
+	a nuestra disposición. Por lo que podemos acceder a ellas tal como si hubiéramos
+	copiado y pegado el código
+*/
+$sentencia = $base_de_datos->prepare("INSERT INTO personas(name, email, number, pass, cpass) VALUES (?, ?, ?, ?, ?);");
+$resultado = $sentencia->execute([$nombre, $email, $telefono, $pass, $cpass]); # Pasar en el mismo orden de los ?
 
-        if (mysqli_query($conex, $sql)) {
-            echo "Registro exitoso.";
-        } else {
-            echo "Error al registrar el usuario: " . mysqli_error($conex);
-        }
-    }
-} else {
-    echo "Acceso denegado.";
-}
+#execute regresa un booleano. True en caso de que todo vaya bien, falso en caso contrario.
+#Con eso podemos evaluar
+
+if($resultado === TRUE) echo "Registro Exitoso";
+else echo "Algo salió mal. Por favor verifica la información ingresada";
+
+
 ?>
